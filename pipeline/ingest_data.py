da51791620991f9e0aef78590b8ea6285c246b53
Henry Okonkwo <111023153+favhenry@users.noreply.github.com>
2026-01-19 13:15:02 +0000

    pipeline

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -29,27 +30,36 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-
-
-
-
-
-def run():
-    pg_user ='root'
-    pg_pass ='root'
-    pg_host ='localhost'
-    pg_port =5432
-    pg_db ='ny_taxi'
-    year=2021
-    month=1
-    target_table = 'yellow_taxi_data'
-    chunksize=100000
+pg_user ='root'
+pg_pass ='root'
+pg_host ='localhost'
+pg_port =5432
+pg_db ='ny_taxi'
+year=2021
+month=1
+target_table = 'yellow_taxi_data'
+chunksize=100000
+@click.command()
+@click.option("--pg_user", default="root", help="Postgres username")
+@click.option("--pg_pass", default="root", help="Postgres password")
+@click.option("--pg_host", default="localhost", help="Postgres host")
+@click.option("--pg_port", default=5432, type=int, help="Postgres port")
+@click.option("--pg_db", default="ny_taxi", help="Postgres database name")
+@click.option("--year", default=2021, type=int, help="Year of dataset")
+@click.option("--month", default=1, type=int, help="Month of dataset")
+@click.option("--target_table", default="yellow_taxi_data", help="Target table name")
+@click.option("--chunksize", default=100000, type=int, help="Chunk size for batch insert")
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url=f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-
-    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-
+    url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
+    try:
+        engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+        print("Database connection successful")
+    except Exception as e:
+        print("Connection failed:", str(e))
+        raise
+    print(f"Starting download from: {url}")
     df_iter=pd.read_csv(
         url,
         dtype=dtype,
@@ -82,8 +92,8 @@ def run():
             name=target_table  ,
             con=engine,
             if_exists="append"
-    )
-    print("Inserted chunk:", len(df_chunk))
+        )
+        print("Inserted chunk:", len(df_chunk))
 
 
 if __name__ == '__main__':
